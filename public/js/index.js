cf15b3152ef5e5b147d281216f43d2d9971bce4e
GitHub <125565937+Git-Hub-25@users.noreply.github.com>
2023-04-28 15:39:40 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import { login, logout } from "./login";
 import { updateSettings } from "./updateSettings";
 import { displayMap } from "./mapbox";
 import { bookTour } from "./stripe";
+import {signup} from "./signup";
 
 const mapBox = document.getElementById("map");
 const loginForm = document.querySelector(".form--login");
