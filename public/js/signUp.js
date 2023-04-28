8c6691b3fb082626054af1ba300a0be7123ff9ec
GitHub <125565937+Git-Hub-25@users.noreply.github.com>
2023-04-28 14:47:52 +0530

    Add files via upload

---
+++ public/js/signUp.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { showAlert } from './alert';
+export const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: 'http://localhost:5000/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm,
+      },
+    });
+    if (response.data.status === 'Success') {
+      showAlert('success', 'Sign Up is successfully');
+      window.setTimeout(() => {
+        location.assign('/login');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
